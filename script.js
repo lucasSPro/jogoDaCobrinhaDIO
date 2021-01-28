be259ce75055bf2b04a356c118821aa502242d1d
Lucas dos Santos <31446385+lucasSPro@users.noreply.github.com>
2021-01-28 00:44:35 -0300

    adiciona colisão e fim do jogo

--- script.js
+++ script.js
@@ -44,6 +44,13 @@ function iniciarGame(){
     if(snake[0].y > 15 * box && direction == "down" ) snake[0].y = 0;
     if(snake[0].y < 0        && direction == "up"   ) snake[0].y = 16 * box;
 
+    for(i = 1; i < snake.length; i++ ){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y ){
+           clearInterval(jogo);
+           alert('Game Over.') 
+        }
+    }
+
     criarBG();
     criarCobrinha();
     criarComida();
@@ -56,7 +63,12 @@ function iniciarGame(){
     if(direction == "up") snakeY -= box;
     if(direction == "down") snakeY += box;
 
-    snake.pop();
+    if(snakeX != comida.x || snakeY != comida.y){
+        snake.pop();
+    }else{
+      comida.x = Math.floor(Math.random() * 15 + 1) * box;
+      comida.y = Math.floor(Math.random() * 15 + 1) * box;
+    }
 
     let newHead = {
         x: snakeX,
